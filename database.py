125e27e8ea62d980134585ca851437fb76bdfbc7
radsylph <andresa@devtop.io>
2023-12-24 00:48:07 -0400

    feat: initial commit

---
+++ database.py
@@ -0,0 +1,74 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from decouple import config
+
+url = config("MONGO_URL")
+
+client = MongoClient(
+    url,
+    server_api=ServerApi("1"),
+    connectTimeoutMS=30000,
+    socketTimeoutMS=None,
+    connect=False,
+    maxPoolsize=1,
+)
+
+db = client["bot_Emperador"]
+collection = db["integrantes"]
+
+
+def agregar_participante(nombre: str) -> str:
+    if nombre == "":
+        return "No se pueden agregar campos vacios"
+
+    if collection.find_one({"nombre": nombre}):
+        return "Ya existe el usuario en el grupo"
+    try:
+        nuevo_participante = {
+            "nombre": nombre,
+        }
+        collection.insert_one(nuevo_participante)
+        return f"Se agrego el participante {nombre} de forma satisfactoria"
+    except:
+        return "No se pudo agregar el participante"
+
+
+def eliminar_participante(nombre: str) -> str:
+    if nombre == "":
+        return "No se pueden agregar campos vacios"
+
+    if collection.find_one({"nombre": nombre}) is None:
+        return "No existe el usuario en el grupo"
+    try:
+        collection.delete_one({"nombre": nombre})
+        return f"Se elimino el participante {nombre} de forma satisfactoria"
+    except:
+        return "No se pudo eliminar el participante"
+
+
+def consultar_participantes() -> str:
+    try:
+        participantes = collection.find()
+        if participantes is None:
+            return "No hay participantes en el grupo"
+        mensaje = " Atencion: \n"
+        for participante in participantes:
+            mensaje += f"@{participante['nombre']} \n"
+        return mensaje
+    except:
+        return "No se pudo consultar los participantes"
+
+
+def borrar_todo(grupo: str) -> str:
+    if grupo == "":
+        return "No se pueden agregar campos vacios"
+
+    participantes = collection.find({"grupo": grupo})
+    if participantes is None:
+        return "No hay participantes en el grupo"
+
+    for participante in participantes:
+        collection.delete_one({"nombre": participante["nombre"], "grupo": grupo})
+    return (
+        f"Se borraron todos los participantes del grupo {grupo} de forma satisfactoria"
+    )
---
+++ main.py
@@ -0,0 +1,107 @@
+import threading
+import time
+import sys
+import traceback
+from typing import Final
+from telegram import Update
+from telegram import Bot
+from telegram.ext import (
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    Updater,
+    CallbackContext,
+)
+from decouple import config
+import sys
+
+from database import (
+    agregar_participante,
+    eliminar_participante,
+    consultar_participantes,
+    borrar_todo,
+)
+
+TOKEN: Final = config("BOT_TOKEN")
+BOTNAME: Final = "@Lord_Emperador_Bot"
+chatId = "-1002126307423"
+
+
+# Bot commands
+def Bot_help_commands(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        """
+    /ayuda: enseña como usar el bot de forma facil y sencilla
+    /agregar <nombre> <grupo>: agrega una persona a la base de datos
+    /eliminar <nombre> <grupo>: elimina una persona de la base de datos \U0001f6a8
+    /lista <grupo>: muestra todos los registros de personas en un grupo
+    /reset <grupo>: CUIDADO elimina todos los registros de personas en un grupo \U0001f6a8
+    """
+    )
+
+
+# Bot Messages
+
+
+def handle_message(update: Update, context: CallbackContext):
+    text: str = update.message.text.lower()
+    print(text)
+    if "@everyone" in text:
+        update.message.reply_text(hanlder_list_participants(update, context))
+    elif "sexo" in text:
+        update.message.reply_text("anal")
+    elif "hola" in text:
+        update.message.reply_text(f"Hola @{update.message.from_user.username}")
+    elif (
+        (update.message.from_user.username == "Vanilthas")
+        and (len(text) > 5)
+        and ("soto" in text)
+    ):
+        print(text)
+        update.message.reply_text("SOTOOOOO")
+
+
+def handler_add_participant(update: Update, context: CallbackContext):
+    nombre = update.message.text.split(" ")[1]
+    # grupo = update.message.text.split(" ")[2]
+    update.message.reply_text(agregar_participante(nombre))
+
+
+def handler_chatId(update: Update, context: CallbackContext):
+    update.message.reply_text(f"el chad id es: {update.message.chat_id}")
+    chatId = update.message.chat_id
+
+
+def hanlder_delete_participant(update: Update, context: CallbackContext):
+    nombre = update.message.text.split(" ")[1]
+    # grupo = update.message.text.split(" ")[2]
+    update.message.reply_text(eliminar_participante(nombre))
+
+
+def hanlder_list_participants(update: Update, context: CallbackContext):
+    update.message.reply_text(consultar_participantes())
+
+
+def get_group_members(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+    members = context.bot.get_chat_members(chatId)
+    for member in members:
+        print(member.user.username)
+        update.message.reply_text(member.user.username)
+
+
+if __name__ == "__main__":
+    print("Starting bot")
+    updater = Updater(token=TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("ayuda", Bot_help_commands))
+    dp.add_handler(CommandHandler("agregar", handler_add_participant))
+    dp.add_handler(CommandHandler("eliminar", hanlder_delete_participant))
+    dp.add_handler(CommandHandler("everyone", hanlder_list_participants))
+    dp.add_handler(CommandHandler("id", handler_chatId))
+    dp.add_handler(CommandHandler("members", get_group_members))
+    dp.add_handler(MessageHandler(Filters.text, handle_message))
+
+    updater.start_polling()
+
+    updater.idle()
