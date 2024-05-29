b7afb62f5436ad27f9e03c2faaac51472580a24f
andresalvarezsilva6 <andresalvarezsilva6@gmail.com>
2024-05-29 19:29:53 +0000

    feat: reworked the way the handle_messages work

--- main.py
+++ main.py
@@ -22,18 +22,16 @@ from database import (
 )
 
 async def handler_chatId(update: Update, context: CallbackContext):
-    await update.message.reply_text(f"el chad id es: {update.message.chat_id}")
-    chatId = await update.message.chat_id
+    await update.message.reply_text(f"el chat id es: {update.message.chat_id}")
+    chatId = update.message.chat_id
 
 def handler_add_participant(update: Update, context: CallbackContext):
     nombre = update.message.text.split(" ")[1]
-    # grupo = update.message.text.split(" ")[2]
     response = agregar_participante(nombre)
     update.message.reply_text(response)
 
-def hanlder_delete_participant(update: Update, context: CallbackContext):
+def handler_delete_participant(update: Update, context: CallbackContext):
     nombre = update.message.text.split(" ")[1]
-    # grupo = await update.message.text.split(" ")[2]
     response = eliminar_participante(nombre)
     update.message.reply_text(response)
 
@@ -55,55 +53,58 @@ def load_json(file):
 
 response_data = load_json("botMessages.json")
 
-def BotMessages(userInput: str) -> str:
+def BotMessages(userInput: str):
     for response in response_data:
         if userInput in response["user_input"]:
-            return response["bot_response"]
-
-
+            return response
 
 # Bot commands
 def Bot_help_commands(update: Update, context: CallbackContext):
     update.message.reply_text(
         """
     /ayuda: enseña como usar el bot de forma facil y sencilla
-        /agregar <persona>: agrega una persona a la base de datos
-        /eliminar <persona>: elimina una persona de la base de datos 
-        /lista: lista los participantes de la base de datos para la pole, shipping, etc
+    /agregar <persona>: agrega una persona a la base de datos
+    /eliminar <persona>: elimina una persona de la base de datos 
+    /lista: lista los participantes de la base de datos para la pole, shipping, etc
     """
     )
 
-
 # Bot Messages
-
 def handle_message(update: Update, context: CallbackContext) -> None:
     text: str = update.message.text.lower()
-    print(update.message.text)
     response = BotMessages(text)
-    print(response)
-    if response == "event_everyone":
-        print("toditos")
-        response = handler_list_participants(update, context)
-        update.message.reply_text(response)
-    update.message.reply_text(response)
+    if not response:
+        return
+
+    if response["bot_response_type"] == "text":
+        update.message.reply_text(response["bot_response"])
+    elif response["bot_response_type"] == "audio":
+        audio_file_path = f'audios/{response["bot_response"]}'
+        if os.path.exists(audio_file_path):
+            update.message.reply_audio(audio=open(audio_file_path, 'rb'))
+        else:
+            update.message.reply_text("El audio no se encuentra disponible.")
+    elif response["bot_response"] == "event_everyone":
+        handler_list_participants(update, context)
+    elif response["bot_response"] == 'event_blackFlash':
+        audios = os.listdir("audios")
+        audio = 'audios/test.mp3'
+        update.message.reply_audio(audio=open(audio, 'rb'))
 
 async def get_group_members(update: Update, context: CallbackContext):
-    chat_id = await update.message.chat_id
+    chat_id = update.message.chat_id
     admins = await context.bot.get_chat_administrators(chat_id)
     message = "los miembros del grupo son: \n"
     for admin in admins:
         message += "@" + admin.user.username + "\n"
     await update.message.reply_text(message)
 
-
 if __name__ == "__main__":
     print("Starting bot")
     updater.dispatcher.add_handler(CommandHandler("ayuda", Bot_help_commands))
     updater.dispatcher.add_handler(CommandHandler("agregar", handler_add_participant))
-    updater.dispatcher.add_handler(CommandHandler("lista" , handler_list_participants))
-    updater.dispatcher.add_handler(
-        CommandHandler("eliminar", hanlder_delete_participant)
-    )
+    updater.dispatcher.add_handler(CommandHandler("lista", handler_list_participants))
+    updater.dispatcher.add_handler(CommandHandler("eliminar", handler_delete_participant))
     updater.dispatcher.add_handler(CommandHandler("id", handler_chatId))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
     print("Polling...")
